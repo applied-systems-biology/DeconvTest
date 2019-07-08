fixed the bug in RMSE/NRMSE computation

DeconvTest/modules/quantification.py
@@ -24,9 +24,10 @@ def compute_accuracy_measures(image, gt_image):
     pandas.DataFrame()
         Data frame containing the values for the computed accuracy measures.
     """
+    img_volume = np.product(gt_image.shape)
     image, gt_image = unify_shape(image, gt_image)  # convert cell images to the same shape
     data = pd.DataFrame()
-    data['RMSE'] = [np.sqrt(np.sum((image - gt_image)**2) / (np.product(image.shape)))]
+    data['RMSE'] = [np.sqrt(np.sum((image - gt_image)**2) / img_volume)]
     data['NRMSE'] = data['RMSE'] / np.max(gt_image) - np.min(gt_image)
     return data
 